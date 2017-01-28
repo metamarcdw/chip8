eec36338674a7d3830f6028253bfc6b13b1d3061
Marc Wood <metamarcdw@gmail.com>
2017-01-28 17:45:32 -0600

    Create some Stack tests.
    Create some tests to confirm behavior of a Stack type object.
    Instantiate a Stack. Test list(), push() and pop().

--- test/test_chip8.py
+++ test/test_chip8.py
@@ -1,2 +1,17 @@
 from context import chip8
 
+def test_stack_class():
+    """ Tests aspects of the Stack class """
+
+    stack = chip8.chip8.Stack(["foo", "bar"])
+
+    list = stack.list()
+    assert list == ["foo", "bar"]
+
+    stack.push("baz")
+    assert stack.list() == ["foo", "bar", "baz"]
+
+    item == stack.pop()
+    assert item == "baz"
+    assert stack.list() == ["foo", "bar"]
+
