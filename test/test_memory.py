c2fcdc2d6fb613cafe7c53e79b8143305a4a17f2
Marc Wood <metamarcdw@gmail.com>
2017-01-29 17:20:55 -0600

    Create Memory class and relevant tests.
    Create tests, initiator and new methods: is_byte() and save().
    Tests cover all new methods and exceptions raised.

---
+++ test/test_memory.py
@@ -0,0 +1,27 @@
+import pytest
+
+from .context import chip8
+
+
+mem = chip8.Memory()
+
+def test_memory_class():
+    """ Tests attributes of the Memory class. """
+    
+    assert isinstance(mem, chip8.Memory)
+    assert isinstance(mem._bytes[0], int)
+    assert len(mem._bytes) == 4096
+
+def test_save_method():
+    mem.save(0xFF, 0x200)
+    assert mem._bytes[0x200] == 0xFF
+
+def test_save_valueerror():
+    with pytest.raises(ValueError):
+        mem.save(0x100, 0x200)
+
+def test_save_indexerror():
+    with pytest.raises(IndexError):
+        mem.save(0xFF, 0x1000)
+
+
