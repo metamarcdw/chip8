06ac3e72ae91217a2641648318504d02b343f2f2
Marc Wood <metamarcdw@gmail.com>
2017-01-28 19:34:53 -0600

    Fleshed out Stack class.
    Added initiator, push and pop. All current tests passing.

--- chip8/chip8.py
+++ chip8/chip8.py
@@ -1,4 +1,20 @@
 
 class Stack:
-    pass
+    """ Stack object. Wraps a list. """
+
+    def __init__(self, list_=[]):
+        """ Stack initiator.
+            Initiate with a given list.
+            Defaults to empty list.
+        """
+        self.list_ = list_
+
+    def push(self, item):
+        """ Push an item onto the stack. """
+        self.list_.append(item)
+
+    def pop(self):
+        """ Pop an item off the top of the stack. """
+        return self.list_.pop()
+
 
